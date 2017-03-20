9a6604060a8d58512339931b892d3e1585bf50aa
norbertilies <norbert_ilies@yahoo.com>
2017-03-20 14:34:35 +0200

    Enemy movement (AI)

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public float moveSpeed;
+    public bool moveRight;
+
+    public Transform wallCheck;
+    public float wallCheckRadius;
+    public LayerMask whatIsWall;
+    private bool hittingWall;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        hittingWall = Physics2D.OverlapCircle(wallCheck.position, wallCheckRadius, whatIsWall);
+
+        if (hittingWall)
+            moveRight = !moveRight;
+
+		if (moveRight)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        } else
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+	}
+}
