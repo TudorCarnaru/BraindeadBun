747deca00e673645a70791a7842c5756925097bb
tcarnaru <tud_underworld@yahoo.com>
2017-03-22 15:59:18 +0200

    PLease work and shit

---
+++ Assets/scirpt.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace UnityStandardAssets._2D
+{
+    public class scirpt : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+
+        void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.tag == "Player")
+            {
+                PlatformerCharacter2D speed = other.GetComponent<PlatformerCharacter2D>();
+                speed.setSpeed();
+                Destroy(gameObject);
+                GetComponent<SpriteRenderer>().enabled = false;
+            }
+        }
+    }
+}
